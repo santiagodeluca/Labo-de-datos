e73347f43c32badaac260bcf109dfd09e68373fa
santiagodeluca <santidelu510@gmail.com>
2025-02-16 21:03:10 -0300

    cc esta y casi todo sql anda parte dos

--- tp1/ejercicios.py
+++ tp1/ejercicios.py
@@ -1,8 +1,55 @@
 import pandas as pd
+import numpy as np
 import duckdb as dd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+#%% ARMAMOS CENTROS_CULTURALES
+def centros_culturales():
+    cc = pd.read_csv("tp1/TablasOriginales/centros_culturales.csv", dtype={'ID_DEPTO': str})
+    mail = cc["Mail "]
+    capacidad = cc["Capacidad"]
+    nombre = cc["Nombre"]
+    id_depto = cc["ID_DEPTO"]
+    
+    centro_cultural_data = []
+
+    for i in range(len(nombre)):
+        linea = []
+        linea.append(i)
+        linea.append(nombre[i])
+        if np.isnan(capacidad[i]) or capacidad[i]==0:
+            # cuando no hay datos de capacidad, asignamos 0 
+            linea.append(0)
+        else:
+            linea.append(int(capacidad[i]))
+        if mail[i] == "s/d" or mail[i] == "-" or type(mail[i])==float:
+            linea.append(np.nan)
+        else:
+            linea_mail=''
+            listo = False
+            j=0
+            while j in range(len(mail[i])) and listo==False: 
+                if mail[i][j]==" ":
+                    linea.append(linea_mail)
+                    listo=True
+                    j=0
+                else:
+                    linea_mail = linea_mail + mail[i][j]
+                    j=j+1
+            if listo==False:
+                linea.append(linea_mail)
+        linea.append(id_depto[i])
+        centro_cultural_data.append(linea)
+    encabezado = ["id","nombre","capacidad","mail","id_depto"]
+    df_cc = pd.DataFrame(centro_cultural_data, columns = encabezado)
+    return(df_cc)
+
+centro_cultural = centros_culturales()
+#        distintos nulls 147,162, 213, 317, 319, 324,325,327,328,332,333,334,338,343,348,351,360, 369...372, 
+#        dos mails 169, 175, 183, 203, 246, 252, 277, 287, 321
+#        no valida 373
+#        \n 942
 #%% ARMAMOS ESTABLECIMIENTO_EDUCATIVO
 def establecimiento_educativo():
     e = pd.read_excel("tp1/TablasOriginales/2022_padron_oficial_establecimientos_educativos.xlsx",skiprows=5)
@@ -43,6 +90,7 @@ def establecimiento_educativo():
     df_establecimiento_educativo['primarias'] = df_establecimiento_educativo['primarias'].astype(int)
     df_establecimiento_educativo['secundarias'] = df_establecimiento_educativo['secundarias'].astype(int)
 
+    df_establecimiento_educativo.loc[df_establecimiento_educativo['id_depto'].str.startswith('02'), 'id_depto'] = '02000'
     return(df_establecimiento_educativo)
 
 establecimiento_educativo = establecimiento_educativo()
@@ -71,6 +119,8 @@ df = df[df['id_depto'] != '00']
 df = df.drop(['%', 'Acumulado %'], axis=1)
 df['Edad'] = df['Edad'].astype(int)
 df['Casos'] =  df['Casos'].str.replace(' ', '').astype(int)
+df.loc[df['id_depto'].str.startswith('02'), 'id_depto'] = '02000'
+
 # Renombramos y agrupamos por grupos etarios
 df = df.rename(columns={"Edad": "edad", "Casos": "habitantes"})
 padron = dd.sql("""
@@ -86,6 +136,8 @@ padron = dd.sql("""
                 FROM  df
                 GROUP BY id_depto, grupo_etario;
                 """).df()
+                
+            
 #padron.to_csv('TablasModelo') para agregar el archivo a la carpeta
 #%% ARMAMOS PROVINCIA Y DEPARTAMENTO
 df = pd.read_csv('tp1/TablasOriginales/centros_culturales.csv', dtype={'ID_PROV': str, 
@@ -94,13 +146,14 @@ provincia = dd.sql("""
                    SELECT DISTINCT ID_PROV AS id, Provincia AS nombre
                    FROM df
                    """).df()
-
+df = df.dropna(subset=['Departamento'])
 departamento = dd.sql("""
                    SELECT DISTINCT ID_DEPTO as id_depto, 
                                    Departamento AS nombre_depto,
                                    ID_PROV AS id_prov
                    FROM df 
                    """).df()
+                   
 #%% Analisis de datos
 #ej i)
 # A cada depto le agregamos provincia y total de ee
@@ -125,7 +178,7 @@ ee_por_depto_niveles_y_prov = dd.sql("""
                       INNER JOIN ee_por_depto_niveles AS ee
                       ON d.id_depto=ee.id_depto;
                       """).df()
-#%% Sacamos del padron las poblaciones                      
+# Sacamos del padron las poblaciones                      
 pob_jardin = dd.sql("""
                       SELECT id_depto, habitantes AS poblacion_jardin
                       FROM padron AS p
@@ -149,7 +202,7 @@ poblaciones = dd.sql("""
                       NATURAL JOIN pob_primaria 
                       NATURAL JOIN pob_secundaria;
                       """).df()
-#%% Armamos la ultima tabla
+# Armamos la ultima tabla
 ee_poblacion_por_depto_y_prov = dd.sql("""
                       SELECT prov_nombre AS Provincia, depto_nombre AS Departamento, 
                              jardines AS Jardines, 
@@ -170,20 +223,20 @@ cc_cap100 = dd.sql("""
                       WHERE capacidad > 100;
                       """).df()    
 cc_cap100_por_depto = dd.sql("""
-                      SELECT id_depto, COUNT(*) AS cant>100
+                      SELECT id_depto, COUNT(*) AS cant
                       FROM cc_cap100 AS cc
                       GROUP BY id_depto;
                       """).df()    
 
 cc_cap100_por_depto_y_prov = dd.sql("""
-                      SELECT prov_nombre, depto_nombre, cant>100
+                      SELECT prov_nombre, depto_nombre, cant
                       FROM depto_y_prov AS d
-                      INNER JOIN cc_cap100_por_depto AS cc
-                      ON d.id_depto=cc.id_depto
-                      ORDER BY prov_nombre, cant>100 DESC;
+                      NATURAL JOIN cc_cap100_por_depto AS cc
+                      ORDER BY prov_nombre, cant DESC;
                       """).df()    
 
 #%% ej iii)
+#FALTA UN DEPTO
 cc_por_depto = dd.sql("""
                       SELECT id_depto, COUNT(*) AS Cant_CC
                       FROM centro_cultural AS cc
@@ -232,7 +285,7 @@ dominio_mas_frecuente = dd.sql("""
                                        );
                       """).df()
 dominio_mas_frecuente_depto_y_prov = dd.sql("""
-                      SELECT prov_nombre, depto_nombre, dominio AS 'Dominio mas frecuente en CC'
+                      SELECT prov_nombre, depto_nombre, dominio AS 'Dominio más frecuente en CC'
                       FROM depto_y_prov 
                       NATURAL JOIN dominio_mas_frecuente;
                       """).df()
@@ -257,34 +310,52 @@ fig, ax = plt.subplots()
 
 ax.scatter(ee_poblacion_por_depto_y_prov['Poblacion jardin'], 
            ee_poblacion_por_depto_y_prov['Jardines'], 
-           color='#A6CEE3', label='Jardines', s=10)
+           color='#1F77B4', label='Jardines', s=14)
 ax.scatter(ee_poblacion_por_depto_y_prov['Poblacion primaria'], 
            ee_poblacion_por_depto_y_prov['Primarias'], 
-           color='#1F78B4', label='Primarias', s=10)
+           color='#FF7F0E', label='Primarias', s=14)
 ax.scatter(ee_poblacion_por_depto_y_prov['Poblacion secundaria'], 
            ee_poblacion_por_depto_y_prov['Secundarias'], 
-           color='#08306B', label='Secundarias', s=10)
+           color='#2CA02C', label='Secundarias', s=14)
 
 ax.set_xlabel("Población")
-ax.set_ylabel("Cantidad establecimientos educativos de tipo común")
-ax.set_title("Cantidad de establecimientos educativos por población")
-ax.set_xlim(0, 80000)
-ax.set_ylim(0, 270)
+ax.set_ylabel("Establecimientos educativos(tipo común)")
+ax.set_title("Establecimientos educativos vs. población")
+#ax.set_xlim(0, 80000)
+#ax.set_ylim(0, 270)
 ax.legend()
 #%%ej iii)
-#tal vez se puede sacar id_depto de aca
 prov_depto_ee = dd.sql("""
-                          SELECT d.id_depto, prov_nombre, Cant_EE AS cant
-                          FROM depto_y_prov AS 
-                          NATURAL JOIN ee_por_depto
+                          SELECT prov_nombre, Cant_EE AS cant
+                          FROM depto_y_prov AS d
+                          NATURAL JOIN ee_por_depto;
                        """).df()
-                       
+ 
+# Acortamos nombres para mejorar la visualizacion
+prov_depto_ee.loc[
+    prov_depto_ee['prov_nombre'] == 'Tierra del Fuego, Antártida e Islas del Atlántico Sur',
+    'prov_nombre'] = 'Tierra del F.' 
+prov_depto_ee.loc[
+    prov_depto_ee['prov_nombre'] == 'Santiago del Estero',
+    'prov_nombre'] = 'Sant. del Est.' 
+#DUDA aca habria que eliminar caba...
+prov_depto_ee.loc[
+    prov_depto_ee['prov_nombre'] == 'Ciudad Autónoma de Buenos Aires',
+    'prov_nombre'] = 'CABA' 
+                      
+# Calculamos medianas y ordenamos 
+#DUDA : no vimos group by...
 medianas = prov_depto_ee.groupby('prov_nombre')['cant'].median()
 provs_ordenadas = medianas.sort_values().index
 
 fig, ax = plt.subplots()
-prov_depto_ee.boxplot(by='prov_nombre', column='cant', ax=ax, 
-                      order=provs_ordenadas)
+sns.boxplot(x='prov_nombre', y='cant', data=prov_depto_ee, order=provs_ordenadas, ax=ax)
+
+ax.set_xlabel("Provincia")
+ax.set_ylabel("Cantidad establecimientos educativos(tipo común)")
+ax.set_title("Establecimientos educativos por departamento")
+ax.set_ylim(0, 750)
+plt.xticks(rotation=80)
 """
 nombres_provincias = dd.sql(
                             SELECT DISTINCT nombre
@@ -292,3 +363,37 @@ nombres_provincias = dd.sql(
                             ).df()
 for indice, valor in nombres_provincias['nombre'].iteritems():
 """
+#%%ej iv)
+cc_cada_1000 = dd.sql("""
+                          SELECT c.id_depto AS id_depto, 
+                                 CASE 
+                                     WHEN p.Poblacion = 0 THEN 0
+                                     ELSE (c.Cant_CC / p.Poblacion)*1000
+                                 AS cant_cc
+                          FROM cc_por_depto AS c
+                          NATURAL JOIN poblacion_por_depto AS p;
+                       """).df()
+ee_cada_1000 = dd.sql("""
+                          SELECT e.id_depto AS id_depto, 
+                                 CASE 
+                                     WHEN p.Poblacion = 0 THEN 0
+                                     ELSE (e.Cant_EE / p.Poblacion)*1000
+                                 AS cant_ee
+                          FROM ee_por_depto AS e
+                          NATURAL JOIN poblacion_por_depto AS p;
+                       """).df()
+ee_y_cc_cada_1000 = dd.sql("""
+                          SELECT e.id_depto AS id_depto, 
+                                 cant_ee, cant_cc
+                          FROM cc_cada_1000 AS c
+                          NATURAL JOIN ee_cada_1000;
+                       """).df()
+                       
+fig, ax = plt.subplots()
+ax.scatter(ee_y_cc_cada_1000['cant_ee'], 
+           ee_y_cc_cada_1000['cant_cc'], 
+           color='pink', s=10)
+
+ax.set_ylabel("Centros culturales cada 1000 habitantes")
+ax.set_xlabel("Establecimientos educativos cada 1000 habitantes")
+ax.set_title("Centros culturales vs. establecimientos educativos")
